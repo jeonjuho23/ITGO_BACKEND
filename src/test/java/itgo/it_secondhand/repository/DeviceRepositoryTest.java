3519ee815829c1ff41f7159d42a061d7dbbfd6d4
jeonjuho23 <wjswngh23@daum.net>
2023-12-18 13:02:54 +0900

    [테스트 코드 수정] - 기기 명 영어로 변경

--- src/test/java/itgo/it_secondhand/repository/DeviceRepositoryTest.java
+++ src/test/java/itgo/it_secondhand/repository/DeviceRepositoryTest.java
@@ -48,93 +48,93 @@ class DeviceRepositoryTest {
         //== Samsung
 
         // 갤럭시 S23
-        data.add(Device.createDevice("갤럭시 S23",1155000,
+        data.add(Device.createDevice("Galaxy S23",1155000,
                 samMobile,
                 LocalDateTime.of(2023, 2,17,0,0,0),
                 "65744f12d2f015c973196256"));
-        data.add(Device.createDevice("갤럭시 S23 울트라",1599400,
+        data.add(Device.createDevice("Galaxy S23 Ultra",1599400,
                 samMobile,
                 LocalDateTime.of(2023, 2,17,0,0,0),
                 "65744e42d2f015c973196254"));
-        data.add(Device.createDevice("갤럭시 S23 플러스",1353000,
+        data.add(Device.createDevice("Galaxy S23 Plus",1353000,
                 samMobile,
                 LocalDateTime.of(2023, 2,17,0,0,0),
                 "65744eb6d2f015c973196255"));
-        data.add(Device.createDevice("갤럭시 S23 FE",847000,
+        data.add(Device.createDevice("Galaxy S23 FE",847000,
                 samMobile,
                 LocalDateTime.of(2023, 12,8,0,0,0),
                 "65744822d2f015c97319624e"));
 
         // 갤럭시 S22
-        data.add(Device.createDevice("갤럭시 S22",999900,
+        data.add(Device.createDevice("Galaxy S22",999900,
                 samMobile,
                 LocalDateTime.of(2022, 2,25,0,0,0),
                 "65745840d2f015c973196263"));
-        data.add(Device.createDevice("갤럭시 S22 플러스",1199000,
+        data.add(Device.createDevice("Galaxy S22 Plus",1199000,
                 samMobile,
                 LocalDateTime.of(2022, 2,25,0,0,0),
                 "657457e2d2f015c973196262"));
-        data.add(Device.createDevice("갤럭시 S22 울트라",1452000,
+        data.add(Device.createDevice("Galaxy S22 Ultra",1452000,
                 samMobile,
                 LocalDateTime.of(2022, 2,25,0,0,0),
                 "6574576dd2f015c973196261"));
 
         // 갤럭시 S21
-        data.add(Device.createDevice("갤럭시 S21",999900,
+        data.add(Device.createDevice("Galaxy S21",999900,
                 samMobile,
                 LocalDateTime.of(2021, 1,29,0,0,0),
                 ""));
-        data.add(Device.createDevice("갤럭시 S21 플러스",1199000,
+        data.add(Device.createDevice("Galaxy S21 Plus",1199000,
                 samMobile,
                 LocalDateTime.of(2021, 1,29,0,0,0),
                 ""));
-        data.add(Device.createDevice("갤럭시 S21 울트라",1452000,
+        data.add(Device.createDevice("Galaxy S21 Ultra",1452000,
                 samMobile,
                 LocalDateTime.of(2021, 1,29,0,0,0),
                 ""));
 
         // 갤럭시 S20
-        data.add(Device.createDevice("갤럭시 S20",1248500,
+        data.add(Device.createDevice("Galaxy S20",1248500,
                 samMobile,
                 LocalDateTime.of(2020, 10,16,0,0,0),
                 ""));
-        data.add(Device.createDevice("갤럭시 S20 플러스",1353000,
+        data.add(Device.createDevice("Galaxy S20 Plus",1353000,
                 samMobile,
                 LocalDateTime.of(2020, 10,16,0,0,0),
                 ""));
-        data.add(Device.createDevice("갤럭시 S20 울트라",1595000,
+        data.add(Device.createDevice("Galaxy S20 Ultra",1595000,
                 samMobile,
                 LocalDateTime.of(2020, 10,16,0,0,0),
                 ""));
-        data.add(Device.createDevice("갤럭시 S20 FE",899800,
+        data.add(Device.createDevice("Galaxy S20 FE",899800,
                 samMobile,
                 LocalDateTime.of(2022, 4,1,0,0,0),
                 ""));
 
         // 갤럭시 Z 폴드
-        data.add(Device.createDevice("갤럭시 Z 폴드 5",2097700,
+        data.add(Device.createDevice("Galaxy Z Fold 5",2097700,
                 samMobile,
                 LocalDateTime.of(2023, 8,11,0,0,0),
                 "65744af9d2f015c973196250"));
-        data.add(Device.createDevice("갤럭시 Z 폴드 4",1998700,
+        data.add(Device.createDevice("Galaxy Z Fold 4",1998700,
                 samMobile,
                 LocalDateTime.of(2022, 8,10,0,0,0),
                 "65745093d2f015c973196258"));
-        data.add(Device.createDevice("갤럭시 Z 폴드 3",1899700,
+        data.add(Device.createDevice("Galaxy Z Fold 3",1899700,
                 samMobile,
                 LocalDateTime.of(2021, 8,11,0,0,0),
                 ""));
 
         // 갤럭시 Z 플립
-        data.add(Device.createDevice("갤럭시 Z 플립 5",1399200,
+        data.add(Device.createDevice("Galaxy Z Flip 5",1399200,
                 samMobile,
                 LocalDateTime.of(2023, 8,11,0,0,0),
                 "65744b93d2f015c973196251"));
-        data.add(Device.createDevice("갤럭시 Z 플립 4",1353000,
+        data.add(Device.createDevice("Galaxy Z Flip 4",1353000,
                 samMobile,
                 LocalDateTime.of(2022, 8,26,0,0,0),
                 "65745135d2f015c973196259"));
-        data.add(Device.createDevice("갤럭시 Z 플립 3",1254000,
+        data.add(Device.createDevice("Galaxy Z Flip 3",1254000,
                 samMobile,
                 LocalDateTime.of(2021, 8,27,0,0,0),
                 ""));
@@ -143,65 +143,65 @@ class DeviceRepositoryTest {
         //== Apple
 
         // 아이폰 15
-        data.add(Device.createDevice("아이폰 15",1400000,
+        data.add(Device.createDevice("IPhone 15",1400000,
                 appleMobile,
                 LocalDateTime.of(2023, 10,13,0,0,0),
                 "65740dedd2f015c973196242"));
-        data.add(Device.createDevice("아이폰 15 플러스",1500000,
+        data.add(Device.createDevice("IPhone 15 Plus",1500000,
                 appleMobile,
                 LocalDateTime.of(2023, 10,13,0,0,0),
                 "65740d4cd2f015c973196241"));
-        data.add(Device.createDevice("아이폰 15 프로",1700000,
+        data.add(Device.createDevice("IPhone 15 Pro",1700000,
                 appleMobile,
                 LocalDateTime.of(2023, 10,13,0,0,0),
                 "65740c8fd2f015c973196240"));
-        data.add(Device.createDevice("아이폰 15 프로 맥스",1900000,
+        data.add(Device.createDevice("IPhone 15 Pro Max",1900000,
                 appleMobile,
                 LocalDateTime.of(2023, 10,13,0,0,0),
                 "657409cbd2f015c97319623f"));
 
         // 아이폰 14
-        data.add(Device.createDevice("아이폰 14",1400000,
+        data.add(Device.createDevice("IPhone 14",1400000,
                 appleMobile,
                 LocalDateTime.of(2022, 10,7,0,0,0),
                 "65741024d2f015c973196245"));
-        data.add(Device.createDevice("아이폰 14 플러스",1500000,
+        data.add(Device.createDevice("IPhone 14 Plus",1500000,
                 appleMobile,
                 LocalDateTime.of(2022, 10,7,0,0,0),
                 "65740fa5d2f015c973196244"));
-        data.add(Device.createDevice("아이폰 14 프로",1700000,
+        data.add(Device.createDevice("IPhone 14 Pro",1700000,
                 appleMobile,
                 LocalDateTime.of(2022, 10,7,0,0,0),
                 ""));
-        data.add(Device.createDevice("아이폰 14 프로 맥스",1900000,
+        data.add(Device.createDevice("IPhone 14 Pro Max",1900000,
                 appleMobile,
                 LocalDateTime.of(2022, 10,7,0,0,0),
                 "65740edad2f015c973196243"));
 
         // 아이폰 13
-        data.add(Device.createDevice("아이폰 13",1230000,
+        data.add(Device.createDevice("IPhone 13",1230000,
                 appleMobile,
                 LocalDateTime.of(2021, 9,15,0,0,0),
                 "6574124bd2f015c973196248"));
-        data.add(Device.createDevice("아이폰 13 미니",1090000,
+        data.add(Device.createDevice("IPhone 13 미니",1090000,
                 appleMobile,
                 LocalDateTime.of(2021, 9,15,0,0,0),
                 "657412e6d2f015c973196249"));
-        data.add(Device.createDevice("아이폰 13 프로",1490000,
+        data.add(Device.createDevice("IPhone 13 Pro",1490000,
                 appleMobile,
                 LocalDateTime.of(2021, 9,15,0,0,0),
                 "657411d2d2f015c973196247"));
-        data.add(Device.createDevice("아이폰 13 프로 맥스",1630000,
+        data.add(Device.createDevice("IPhone 13 Pro Max",1630000,
                 appleMobile,
                 LocalDateTime.of(2021, 9,15,0,0,0),
                 "65741160d2f015c973196246"));
 
         // 아이폰 SE
-        data.add(Device.createDevice("아이폰 SE 3",730000,
+        data.add(Device.createDevice("IPhone SE 3",730000,
                 appleMobile,
                 LocalDateTime.of(2022, 3,8,0,0,0),
                 ""));
-        data.add(Device.createDevice("아이폰 SE 2",620000,
+        data.add(Device.createDevice("IPhone SE 2",620000,
                 appleMobile,
                 LocalDateTime.of(2020, 4,15,0,0,0),
                 ""));
@@ -210,13 +210,13 @@ class DeviceRepositoryTest {
         //== 태블릿 ==//
 
         //== Samsung
-        data.add(Device.createDevice("갤럭시 탭",620000,
+        data.add(Device.createDevice("Galaxy Tab",620000,
                 samTablet,
                 LocalDateTime.of(2023, 2,17,0,0,0),
                 ""));
 
         //== Apple
-        data.add(Device.createDevice("아이패드",620000,
+        data.add(Device.createDevice("IPad",620000,
                 appleTablet,
                 LocalDateTime.of(2023, 2,17,0,0,0),
                 ""));
